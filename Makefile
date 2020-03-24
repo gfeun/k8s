996755ca39febb5850abdbe709ce22a6a328d0b6
gfeun <gfeun@protonmail.com>
2020-03-24 23:40:26 +0100

    Integrates Kubespray for automated K8S installation

--- Makefile
+++ Makefile
@@ -7,8 +7,10 @@ endif
 .PHONY: create_cluster
 create_cluster:
 	terraform apply -var do_token=${DO_TOKEN} -input=false -auto-approve
-	terraform-inventory --inventory ./ > hosts
+	TF_HOSTNAME_KEY_NAME=name terraform-inventory --inventory ./ > hosts
+	cat kubespray-hosts >> hosts
 	ansible-playbook -i hosts -u root --private-key ~/.ssh/id_rsa cluster_install.yml
+	scp root@controller-1.k8s.hackervaillant.eu:/etc/kubernetes/admin.conf .
 
 .PHONY: destroy_cluster
 destroy_cluster:
--- cluster.tf
+++ cluster.tf
@@ -5,17 +5,22 @@ variable "nb_controllers" {
 
 variable "nb_workers" {
   type = number
-  default = 2
+  default = 3
 }
 
 data "digitalocean_ssh_key" "default" {
   name = "default"
 }
 
+data "digitalocean_domain" "k8s" {
+  name = "k8s.hackervaillant.eu"
+}
+
+
 resource "digitalocean_droplet" "controllers" {
   count = var.nb_controllers
 
-  name = "controller-${count.index}"
+  name = "controller-${count.index}.k8s.hackervaillant.eu"
   image = "debian-10-x64"
   region = "ams3"
   size = "s-1vcpu-3gb"
@@ -25,9 +30,27 @@ resource "digitalocean_droplet" "controllers" {
 resource "digitalocean_droplet" "workers" {
   count = var.nb_workers
 
-  name = "worker-${count.index}"
+  name = "worker-${count.index}.k8s.hackervaillant.eu"
   image = "debian-10-x64"
   region = "ams3"
   size = "s-1vcpu-3gb"
   ssh_keys = [data.digitalocean_ssh_key.default.fingerprint]
+}
+
+resource "digitalocean_record" "controllers" {
+  count = var.nb_controllers
+
+  domain = data.digitalocean_domain.k8s.name
+  type   = "A"
+  name   = "controller-${count.index}"
+  value  = digitalocean_droplet.controllers[count.index].ipv4_address
+}
+
+resource "digitalocean_record" "workers" {
+  count = var.nb_workers
+
+  domain = data.digitalocean_domain.k8s.name
+  type   = "A"
+  name   = "worker-${count.index}"
+  value  = digitalocean_droplet.workers[count.index].ipv4_address
 }
